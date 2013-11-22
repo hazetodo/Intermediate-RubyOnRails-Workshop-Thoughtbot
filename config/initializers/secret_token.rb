30bad75d7ef9faeb5af3be7ce96fa8189f18d5cd
Stefanos Ioannou <stefioanec@gmail.com>
2013-11-22 21:11:52 +0000

    Initial commit - User model, dashboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntermediateRubyonrailsWorkshopThoughtbot::Application.config.secret_token = '00f12863c7f3cce67855c790591d3feaf6a782193c27d73655433611871e123bdb181d9d88d0c80914cce477455a4d3ccf8c295a726b1051d5e859d4497eb4df'
